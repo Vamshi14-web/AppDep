2d5002236fc3d0ea3c16cd5ac3b6c735ffd27e1e
vamshi vanaparthi <vamshivanaparthi@192.168.1.7>
2026-01-08 11:52:21 +0530

    fix the  error in index

--- Server.js
+++ Server.js
@@ -16,7 +16,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/*", (req, res) => {
+app.use("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
